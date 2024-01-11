b4174fecb912c1bf9f05d410a6506208642313ec
nquidox <40396820+nquidox@users.noreply.github.com>
2024-01-11 22:51:27 +0300

    ToCamelCase

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func ToCamelCase(s string) string {
+	/*
+		Complete the method/function so that it converts dash/underscore delimited words into camel casing.
+		The first word within the output should be capitalized only if the original word was capitalized
+		(known as Upper Camel Case, also often referred to as Pascal case).
+		The next words should be always capitalized.
+	*/
+	runes := []rune(s)
+	var newString []rune
+	for i := 0; i < len(runes); i++ {
+		if runes[i] == '_' || runes[i] == '-' {
+			runes[i+1] = unicode.ToUpper(runes[i+1])
+		} else {
+			newString = append(newString, runes[i])
+		}
+
+	}
+
+	return string(newString)
+}
+
+func main() {
+	fmt.Println("Codewars")
+	fmt.Println(ToCamelCase("to_camel-case"))
+}
