52bea02da0e9cd5f40064ab8077353e3afa8e5d2
nquidox <40396820+nquidox@users.noreply.github.com>
2024-01-12 21:24:32 +0300

    Multiple3And5

--- main.go
+++ main.go
@@ -26,7 +26,24 @@ func ToCamelCase(s string) string {
 	return string(newString)
 }
 
+func Multiple3And5(number int) int {
+	/*
+		If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+		The sum of these multiples is 23.
+		Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+		Note: If the number is a multiple of both 3 and 5, only count it once.
+	*/
+	acc := 0
+	for i := 0; i < number; i++ {
+		if i%3 == 0 || i%5 == 0 {
+			acc += i
+		}
+	}
+	return acc
+}
+
 func main() {
 	fmt.Println("Codewars")
 	fmt.Println(ToCamelCase("to_camel-case"))
+	Multiple3And5(10)
 }
