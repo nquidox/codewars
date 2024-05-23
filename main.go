d51374c20ca3929c5a7e3b269e0c305f08c6d5fb
nquidox <nquidox@gmail.com>
2024-05-23 21:02:21 +0300

    Sort Numbers

--- main.go
+++ main.go
@@ -1733,9 +1733,15 @@ func TwoOldestAges(ages []int) [2]int {
 	return [2]int{ages[len(ages)-2], ages[len(ages)-1]}
 }
 
+func SortNumbers(numbers []int) []int {
+	sort.Ints(numbers)
+	return numbers
+}
+
 func main() {
 	fmt.Println("Codewars")
-	fmt.Println(TwoOldestAges([]int{39, 53, 83, 51, 59, 61, 95, 23, 99, 49}), "[95 99]")
+	fmt.Println(SortNumbers([]int{1, 2, 10, 50, 5}), "[1 2 5 10 50]")
+	//fmt.Println(TwoOldestAges([]int{39, 53, 83, 51, 59, 61, 95, 23, 99, 49}), "[95 99]")
 	//fmt.Println(Angle(3), 180)
 	//fmt.Println(Add(1)(3), 4)
 	//fmt.Println(Calc("F7$&QE?M"), 18)
